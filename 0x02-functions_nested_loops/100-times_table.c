5cc995a74f71215f1161d2df5d1598e247039005
Afework <afe.derb1234@gmail.com>
2022-04-12 14:03:56 -0700

    Task Funny

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,61 @@
 #include "main.h"
 
+
 /**
- *print_times_table - print time table.
- * @n: integer.
- *Return:void.
+ * print_spaces - This functions prints commas and spaces
+ * @n: num spaces
+ */
+void print_spaces(int n)
+{
+	int i;
+
+	_putchar(',');
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+}
+
+/**
+ * print_times_table - This functions prints times table
+ * @n: the table to print
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int table, number;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+	for (table = 0; table <= n; table++)
 	{
-		for (i = 0; i <= n; i++)
+		for (number = 0; number <= n; number++)
 		{
-			_putchar('0');
-			if (n != 0)
-				_putchar(',');
-			for (j = 1; j <= n; j++)
+			int r;
+
+			r = table * number;
+			if (number && r < 10)
+				print_spaces(3);
+			else if (r >= 10 && r < 100)
+				print_spaces(2);
+			else if (r >= 100)
+				print_spaces(1);
+			if (r >= 10 && r < 100)
 			{
-				k = i * j;
-				if (k < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + k);
-				}
-				else if (k < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k % 100) / 10) + '0');
-					_putchar(((k % 100) % 10) + '0');
-				}
-				if (j < n)
-					_putchar(',');
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
 			}
-			_putchar('\n');
+			else if (r >= 100)
+			{
+				_putchar((r / 100) + '0');
+				_putchar(((r / 10) % 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			else
+			{
+				_putchar(r + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
